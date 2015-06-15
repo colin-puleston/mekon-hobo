6c520fd52c2c1caea29d0b2000f66f0f621d5d42
ColinPuleston <puleston@manchester.ac.uk>
2015-06-15 11:22:05 +0100

    Removed CFrame definition stuff

--- mekon-gui/src/uk/ac/manchester/cs/mekon/gui/ModelFrameSelectionPanel.java
+++ mekon-gui/src/uk/ac/manchester/cs/mekon/gui/ModelFrameSelectionPanel.java
@@ -37,7 +37,6 @@ class ModelFrameSelectionPanel extends JPanel {
 	static private final long serialVersionUID = -1;
 
 	static private final String DETAILS_TITLE = "Details";
-	static private final String DEFINITIONS_TITLE = "Definitions";
 	static private final String USAGE_TITLE = "Usage";
 	static private final String ANNOTATIONS_TITLE = "Annotations";
 
@@ -117,7 +116,6 @@ class ModelFrameSelectionPanel extends JPanel {
 		JTabbedPane panel = new JTabbedPane();
 
 		panel.addTab(DETAILS_TITLE, createDetailsPanel(selected));
-		panel.addTab(DEFINITIONS_TITLE, createDefinitionsPanel(selected));
 		panel.addTab(USAGE_TITLE, createUsagePanel(selected));
 		panel.addTab(ANNOTATIONS_TITLE, createAnnotationsPanel(selected));
 
@@ -135,11 +133,6 @@ class ModelFrameSelectionPanel extends JPanel {
 		tree.addSelectionListener(reselectionListener);
 
 		return new JScrollPane(tree);
-	}
-
-	private JComponent createDefinitionsPanel(CFrame selected) {
-
-		return new JScrollPane(new CFramesList(selected.getDefinitions()));
 	}
 
 	private JComponent createUsagePanel(CFrame selected) {
