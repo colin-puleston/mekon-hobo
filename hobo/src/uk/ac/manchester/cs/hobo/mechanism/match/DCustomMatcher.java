fc310aa1050b7f11cfd73e70acdfedc069c758b0
ColinPuleston <puleston@manchester.ac.uk>
2015-12-17 17:49:48 +0000

    Minor comment change

--- hobo/src/uk/ac/manchester/cs/hobo/mechanism/match/DCustomMatcher.java
+++ hobo/src/uk/ac/manchester/cs/hobo/mechanism/match/DCustomMatcher.java
@@ -249,7 +249,7 @@ public class DCustomMatcher implements IMatcher {
 	 *
 	 * @param query Query to be matched
 	 * @param instance Instance to test for matching
-	 * @return True if query matched by query instance
+	 * @return True if query matched by instance
 	 */
 	public boolean matches(IFrame query, IFrame instance) {
 
