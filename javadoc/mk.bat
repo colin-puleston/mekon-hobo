d374dbca68be1927319dc11bab17d9fe9dcef661
ColinPuleston <puleston@manchester.ac.uk>
2016-09-26 16:08:23 +0100

    Updated javadoc/mk.bat for updated OWL API jar

--- javadoc/mk.bat
+++ javadoc/mk.bat
@@ -1,2 +1,2 @@
 rmdir /S /Q mekon-hobo
-javadoc -classpath ..\lib\owlapi-bin.jar;..\lib\owlapitools-bin.jar;..\lib\HermiT.jar -d mekon-hobo -overview mekon-hobo-overview.html -sourcepath ..\mekon\src;..\mekon-owl\src;..\hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MK-OUTPUT
+javadoc -classpath ..\lib\owlapi\*.jar -d mekon-hobo -overview mekon-hobo-overview.html -sourcepath ..\mekon\src;..\mekon-owl\src;..\hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MK-OUTPUT
