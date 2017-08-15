4b94c0df2e083690226a6775cd1bbae74e87de18
Colin-Puleston <colins.email@virgin.net>
2017-08-15 14:44:09 +0100

    Updated classpath in javadoc/mk.bat

--- javadoc/mk.bat
+++ javadoc/mk.bat
@@ -1,2 +1,2 @@
 rmdir /S /Q mekon-hobo
-javadoc -classpath ..\lib\owlapi\*.jar -d mekon-hobo -overview mekon-hobo-overview.html -sourcepath ..\mekon\src;..\mekon-owl\src;..\hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MK-OUTPUT
+javadoc -classpath ..\lib\owlapi\*;..\lib\owlapi\dependencies\*;..\lib\util\* -d mekon-hobo -overview mekon-hobo-overview.html -sourcepath ..\mekon\src;..\mekon-owl\src;..\hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MK-OUTPUT
