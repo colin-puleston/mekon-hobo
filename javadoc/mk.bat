6c6bd7e70d91acaa53f989bb942f21dbf7fe6778
Colin-Puleston <colins.email@virgin.net>
2019-06-13 16:25:22 +0100

    Remove javadoc/mk.bat

--- javadoc/mk.bat
+++
@@ -1,2 +0,0 @@
-rmdir /S /Q mekon-hobo
-javadoc -classpath ..\lib\owlapi\*;..\lib\owlapi\dependencies\*;..\lib\util\* -d mekon-hobo -overview mekon-hobo-overview.html -sourcepath ..\mekon\src;..\mekon-owl\src;..\hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MK-OUTPUT
