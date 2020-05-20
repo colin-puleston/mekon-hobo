0de8f1f34dc2a088fabae6870ba71905921c7d19
Colin-Puleston <colins.email@virgin.net>
2020-05-20 11:33:20 +0100

    MEKON: Modified DemoModelIds for updated demo model

--- mekon/test/uk/ac/manchester/cs/mekon/demomodel/DemoModelIds.java
+++ mekon/test/uk/ac/manchester/cs/mekon/demomodel/DemoModelIds.java
@@ -31,99 +31,77 @@ import uk.ac.manchester.cs.mekon.model.*;
  */
 public class DemoModelIds {
 
-	static public final CIdentity CORE_CONCEPT = coreId("CoreConcept");
-	static public final CIdentity DYNAMIC_CONCEPT = coreId("DynamicConcept");
+	static public final CIdentity CORE_CONCEPT = createId("CoreConcept");
+	static public final CIdentity CONTENT_CONCEPT = createId("ContentConcept");
 
-	static public final CIdentity INTER_CORE_PROPERTY = coreId("interCoreProperty");
-	static public final CIdentity NUMERIC_PROPERTY = coreId("numericValue");
+	static public final CIdentity INTER_CORE_PROPERTY = createId("interCoreProperty");
+	static public final CIdentity NUMERIC_PROPERTY = createId("numericValue");
 
-	static public final CIdentity CITIZEN = coreId("Citizen");
-	static public final CIdentity EMPLOYED_CITIZEN = coreId("EmployedCitizen");
-	static public final CIdentity UNEMPLOYED_CITIZEN = coreId("UnemployedCitizen");
+	static public final CIdentity CITIZEN = createId("Citizen");
+	static public final CIdentity EMPLOYED_CITIZEN = createId("EmployedCitizen");
+	static public final CIdentity UNEMPLOYED_CITIZEN = createId("UnemployedCitizen");
 
-	static public final CIdentity PERSONAL = coreId("Personal");
-	static public final CIdentity TAX = coreId("Tax");
-	static public final CIdentity ZERO_TAX = coreId("ZeroTax");
-	static public final CIdentity BENEFIT = coreId("Benefit");
-	static public final CIdentity UNEMPLOYMENT_BENEFIT = coreId("UnemploymentBenefit");
-	static public final CIdentity EMPLOYMENT = coreId("Employment");
+	static public final CIdentity PERSONAL = createId("Personal");
+	static public final CIdentity TAX = createId("Tax");
+	static public final CIdentity ZERO_TAX = createId("ZeroTax");
+	static public final CIdentity BENEFIT = createId("Benefit");
+	static public final CIdentity UNEMPLOYMENT_BENEFIT = createId("UnemploymentBenefit");
+	static public final CIdentity EMPLOYMENT = createId("Employment");
 
-	static public final CIdentity JOB = coreId("Job");
-	static public final CIdentity JOB_TYPE = coreId("JobType");
-	static public final CIdentity INDUSTRY = coreId("Industry");
-	static public final CIdentity SECTOR = coreId("IndustrySector");
+	static public final CIdentity JOB = createId("Job");
+	static public final CIdentity JOB_TYPE = createId("JobType");
+	static public final CIdentity INDUSTRY = createId("Industry");
+	static public final CIdentity SECTOR = createId("IndustrySector");
 
-	static public final CIdentity TRAVEL_CLASS = coreId("TravelClass");
+	static public final CIdentity TRAVEL_CLASS = createId("TravelClass");
 
-	static public final CIdentity TAX_PROPERTY = coreId("taxPaid");
-	static public final CIdentity BENEFIT_PROPERTY = coreId("benefitReceived");
-	static public final CIdentity EMPLOYMENT_PROPERTY = coreId("employment");
-	static public final CIdentity JOB_PROPERTY = coreId("job");
-	static public final CIdentity INDUSTRY_PROPERTY = coreId("industry");
-	static public final CIdentity JOB_TYPE_PROPERTY = coreId("jobType");
-	static public final CIdentity PAY_RATE_PROPERTY = coreId("hourlyPay");
-	static public final CIdentity SECTOR_PROPERTY = coreId("sector");
-	static public final CIdentity TEACHES_PROPERTY = coreId("teaches");
-	static public final CIdentity NAME_PROPERTY = coreId("name");
-	static public final CIdentity AGE_PROPERTY = coreId("age");
-	static public final CIdentity ADDRESS_PROPERTY = coreId("address");
-	static public final CIdentity LOCATION_PROPERTY = coreId("location");
-	static public final CIdentity TRAVEL_CLASS_PROPERTY = coreId("travelClass");
+	static public final CIdentity TAX_PROPERTY = createId("taxPaid");
+	static public final CIdentity BENEFIT_PROPERTY = createId("benefitReceived");
+	static public final CIdentity EMPLOYMENT_PROPERTY = createId("employment");
+	static public final CIdentity JOB_PROPERTY = createId("job");
+	static public final CIdentity INDUSTRY_PROPERTY = createId("industry");
+	static public final CIdentity JOB_TYPE_PROPERTY = createId("jobType");
+	static public final CIdentity PAY_RATE_PROPERTY = createId("hourlyPay");
+	static public final CIdentity SECTOR_PROPERTY = createId("sector");
+	static public final CIdentity TEACHES_PROPERTY = createId("teaches");
+	static public final CIdentity NAME_PROPERTY = createId("name");
+	static public final CIdentity AGE_PROPERTY = createId("age");
+	static public final CIdentity ADDRESS_PROPERTY = createId("address");
+	static public final CIdentity LOCATION_PROPERTY = createId("location");
+	static public final CIdentity TRAVEL_CLASS_PROPERTY = createId("travelClass");
 
-	static public final CIdentity SPECIALIST = dynamicId("Specialist");
-	static public final CIdentity ACADEMIC_JOB = dynamicId("AcademicJob");
-	static public final CIdentity TEACHER = dynamicId("Teacher");
-	static public final CIdentity LECTURER = dynamicId("Lecturer");
-	static public final CIdentity RESEARCHER = dynamicId("Researcher");
-	static public final CIdentity MEDIC = dynamicId("Medic");
-	static public final CIdentity DOCTOR = dynamicId("Doctor");
-	static public final CIdentity NURSE = dynamicId("Nurse");
-	static public final CIdentity PHYSIO = dynamicId("Physio");
+	static public final CIdentity SPECIALIST = createId("Specialist");
+	static public final CIdentity ACADEMIC_JOB = createId("AcademicJob");
+	static public final CIdentity TEACHER = createId("Teacher");
+	static public final CIdentity LECTURER = createId("Lecturer");
+	static public final CIdentity RESEARCHER = createId("Researcher");
+	static public final CIdentity MEDIC = createId("Medic");
+	static public final CIdentity DOCTOR = createId("Doctor");
+	static public final CIdentity NURSE = createId("Nurse");
+	static public final CIdentity PHYSIO = createId("Physio");
 
-	static public final CIdentity ACADEMIA = dynamicId("Academia");
-	static public final CIdentity HEALTH = dynamicId("Health");
+	static public final CIdentity ACADEMIA = createId("Academia");
+	static public final CIdentity HEALTH = createId("Health");
 
-	static public final CIdentity PUBLIC = dynamicId("Public");
-	static public final CIdentity PRIVATE = dynamicId("Private");
+	static public final CIdentity PUBLIC = createId("Public");
+	static public final CIdentity PRIVATE = createId("Private");
 
-	static public final CIdentity STUDENT = dynamicId("Student");
-	static public final CIdentity UNI_STUDENT = dynamicId("UniversityStudent");
-	static public final CIdentity POSTGRAD = dynamicId("Postgraduate");
-	static public final CIdentity UNDERGRAD = dynamicId("Undergraduate");
+	static public final CIdentity STUDENT = createId("Student");
+	static public final CIdentity UNI_STUDENT = createId("UniversityStudent");
+	static public final CIdentity POSTGRAD = createId("Postgraduate");
+	static public final CIdentity UNDERGRAD = createId("Undergraduate");
 
-	static public final CIdentity EU = dynamicId("EU");
-	static public final CIdentity UK = dynamicId("UK");
-	static public final CIdentity ENGLAND = dynamicId("England");
+	static public final CIdentity EU = createId("EU");
+	static public final CIdentity UK = createId("UK");
+	static public final CIdentity ENGLAND = createId("England");
 
-	static public final CIdentity TRAIN_TRIP = dynamicId("TrainTrip");
+	static public final CIdentity TRAIN_TRIP = createId("TrainTrip");
 
-	static public final CIdentity ACADEMIC_TEACHING_JOB = dynamicExtnId("AcademicTeachingJob");
-	static public final CIdentity RESEARCH_JOB = dynamicExtnId("ResearchJob");
+	static public final CIdentity ACADEMIC_TEACHING_JOB = createId("AcademicTeachingJob");
+	static public final CIdentity RESEARCH_JOB = createId("ResearchJob");
 
-	static private final String IDENTIFIER_FORMAT = "http://mekon/demo-%s.owl#%s";
+	static private CIdentity createId(String name) {
 
-	static private CIdentity coreId(String name) {
-
-		return createId("core", name);
-	}
-
-	static private CIdentity dynamicId(String name) {
-
-		return createId("dynamic", name);
-	}
-
-	static private CIdentity dynamicExtnId(String name) {
-
-		return createId("dynamic-extn", name);
-	}
-
-	static private CIdentity createId(String fileSuffix, String name) {
-
-		return new CIdentity(getIdentifier(fileSuffix, name), name);
-	}
-
-	static private String getIdentifier(String fileSuffix, String name) {
-
-		return String.format(IDENTIFIER_FORMAT, fileSuffix, name);
+		return new CIdentity("http://mekon/demo.owl#" + name, name);
 	}
 }
