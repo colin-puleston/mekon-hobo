b8c1c6abdc3b81532d6405f14e18d3f759cf881c
Colin-Puleston <colins.email@virgin.net>
2021-07-12 15:22:34 +0100

    GENERAL: Updated mkjavadoc.bat to use targets from latest build scripts

--- mkjavadoc.bat
+++ mkjavadoc.bat
@@ -1,3 +1,4 @@
 rmdir /S /Q javadoc\mekon-hobo
-call ant library
+call ant core
+call ant plugins
 javadoc -classpath build\lib\* -d javadoc\mekon-hobo -overview javadoc\mekon-hobo-overview.html -sourcepath mekon\src;mekon-owl\src;hobo\src -subpackages uk -exclude org.semanticweb.owlapi.* 2> MKJAVADOC-OUTPUT
